a443782af13921796565e1d0614919c844285d08
amin hamed <amin.hamed45@gmail.com.com>
2024-01-07 21:58:58 +0100

    jcp

--- src/main/java/com/example/fer/Service/ServiceImpl.java
+++ src/main/java/com/example/fer/Service/ServiceImpl.java
@@ -1,4 +1,9 @@
 package com.example.fer.Service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
 public class ServiceImpl implements Iservice{
 }
